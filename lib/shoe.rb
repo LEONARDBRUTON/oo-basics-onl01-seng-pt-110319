a8e791f4c1d278c56b203160e732f4e37a3c31aa
Tammy L Leonard-Bruton <enigmalb@outlook.com>
2019-12-02 07:27:48 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -8,16 +8,16 @@
 
 
 class Shoe
-  attr_accessor :color   
-
+  attr_accessor :color :size   
+  attr_reader :brand
  def initialize(brand)
    @brand = brand
  end
- def brand
- end
+ 
+ 
  
 
 end
 
-
+  
 Shoe.new("Adidas")
